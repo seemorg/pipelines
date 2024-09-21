8392ec757fb11fd639385a3100c716134c9c271b
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2024-09-21 18:21:22 +0300

    update worker config

--- apps/ai-pipeline/src/book-worker.ts
+++ apps/ai-pipeline/src/book-worker.ts
@@ -1,83 +1,16 @@
+import path from "path";
 import { Worker } from "bullmq";
 
 import type { BookQueueData } from "./book-queue";
 import { BOOKS_QUEUE_NAME, BOOKS_QUEUE_REDIS } from "./book-queue";
-import { indexOpenitiBook } from "./indexer/openiti";
-import { indexTurathBook } from "./indexer/turath";
-import { db } from "./lib/db";
-import { getBooksData } from "./lib/services/books";
-
-const booksData = await getBooksData();
-
-const aiSupportedBooks = new Set(
-  booksData.filter((b) => b.flags.aiSupported).map((b) => b.id),
-);
-
-const updateBookFlags = async (
-  book: {
-    id: string;
-    flags: PrismaJson.BookFlags;
-  },
-  versionId: string,
-) => {
-  // update book flags
-  await db.book.update({
-    where: {
-      id: book.id,
-    },
-    data: {
-      flags: {
-        ...book.flags,
-        aiSupported: true,
-        aiVersion: versionId,
-      } as PrismaJson.BookFlags,
-    },
-  });
-  aiSupportedBooks.add(book.id);
-};
 
 export const worker = new Worker<BookQueueData>(
   BOOKS_QUEUE_NAME,
-  async (job) => {
-    const { id } = job.data;
-    const book = booksData.find((b) => b.id === id);
-
-    if (!book) {
-      throw new Error(`Book not found: ${id}`);
-    }
-
-    if (aiSupportedBooks.has(id)) {
-      return { skipped: true };
-    }
-
-    const hasTurathBook = !!book.versions.find((v) => v.source === "turath");
-
-    if (hasTurathBook) {
-      const result = await indexTurathBook({ id });
-
-      if (result.status === "success") {
-        await updateBookFlags(book, String(result.versionId!));
-      }
-
-      return result;
-    }
-
-    // index openiti
-    const result = await indexOpenitiBook({ id });
-    if (result.status !== "success" && result.status !== "skipped") {
-      throw new Error(JSON.stringify(result));
-    }
-
-    if (result.status === "success") {
-      await updateBookFlags(book, result.versionId!);
-    }
-
-    return result;
-  },
+  path.resolve(`dist/books-processor.js`),
   {
     connection: BOOKS_QUEUE_REDIS,
-    concurrency: 3,
+    concurrency: 5,
     lockDuration: 1000 * 60 * 40, // 40 minutes
-    stalledInterval: 1000 * 60 * 40, // 40 minutes
+    // stalledInterval: 1000 * 60 * 40, // 40 minutes
   },
 );
--- apps/ai-pipeline/tsup.config.ts
+++ apps/ai-pipeline/tsup.config.ts
@@ -2,7 +2,7 @@ import type { Options } from "tsup";
 import { defineConfig } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entryPoints: ["src/index.ts"],
+  entryPoints: ["src/index.ts", "books-processor.ts"],
   clean: true,
   format: "esm",
   platform: "node",
