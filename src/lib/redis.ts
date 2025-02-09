4c9e02f5cb43133e5e1fe7b7ea72219e85208f1a
ahmedriad1 <ahmed.riad.elashrey@gmail.com>
2025-02-10 00:10:51 +0300

    updates

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -5,7 +5,4 @@ import { env } from "../env";
 export const createRedis = () =>
   new Redis(env.REDIS_URL, {
     maxRetriesPerRequest: null,
-    tls: {
-      rejectUnauthorized: false,
-    },
   });
